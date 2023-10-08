df8822d404dfbf72e4f159ab061a0a7de0871081
Muhammad Aitisam Yaseen <145204832+MaitisamY@users.noreply.github.com>
2023-10-09 02:24:52 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector('button').addEventListener('click', function() {
+    document.querySelector('.overlay').style.display ='block';
+});
+document.querySelector('a').addEventListener('click', function() {
+    document.querySelector('.overlay').style.display ='none';
+});
+
